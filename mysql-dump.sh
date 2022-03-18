bac3a4357d89a89436f3b8ee091796be7121231b
Nunzio Napolitano <napoli430@gmail.com>
2022-03-18 18:42:20 +0100

    primo_test_funzionamento

--- mysql-dump.sh
+++ mysql-dump.sh
@@ -11,69 +11,54 @@
 
 CONFIGFILE=/etc/database_dump/settings.conf
 source $CONFIGFILE
-CREDENTIALS="--defaults-file=$CREDENTIAL_FILE"
+MYSQLCREDENTIAL="--defaults-file=$MYSQLCREDENTIAL_FILE"
 
-
-DATE_FORMAT='%Y%m%d'
-CURRENT_DATE=$(date +"${DATE_FORMAT}")
-CURRENT_TIME=$(date +"%H%M")
-LOGFILENAME=$LOG_PATH/database_dump-${CURRENT_DATE}-${CURRENT_TIME}.log
-
-#SETUP DIRECTORY AND FILEMANE
+###  SETUP DIRECTORY AND FILEMANE
 dirDaily=$LOCAL_BACKUP_DIR/daily
 dirMonthly=$LOCAL_BACKUP_DIR/monthly
 dirYearly=$LOCAL_BACKUP_DIR/yearly
-filenameDaily=backup-$(date +%Y-%m-%d)
-filenameMonthly=backup-$(date +%Y-%m)
-filenameYearly=backup-$(date +%Y)
-sqlfile=$dirDaily/$filenameDaily.sql
-zipfile=$dirDaily/$filenameDaily.zip
+filenameDaily=bck-$(date +%Y-%m-%d)
+filenameMonthly=bck-$(date +%Y-%m)
+filenameYearly=bck-$(date +%Y)
 logfile=$LOCAL_BACKUP_DIR/history.log
 # Make sure directories exists
+[ ! -d $LOCAL_BACKUP_DIR ] mkdir -p $LOCAL_BACKUP_DIR
 mkdir -p $dirDaily
 mkdir -p $dirMonthly
 mkdir -p $dirYearly
 
+# IF not exist  heade di history, copialo
+echo "" >> ${logfile}
+echo "<<<<<<   Database Dump Report :: `date +%D`  >>>>>>" >> ${logfile}
+echo "" >> ${logfile}
+echo "DB Name  :: DB Size   Filename" >> ${logfile}
 
-
-
-[ ! -d $LOG_PATH ] && ${MKDIR} -p ${LOG_PATH}
-echo "" > ${LOGFILENAME}
-echo "<<<<<<   Database Dump Report :: `date +%D`  >>>>>>" >> ${LOGFILENAME}
-echo "" >> ${LOGFILENAME}
-echo "DB Name  :: DB Size   Filename" >> ${LOGFILENAME}
 
 ### Make a backup ###
-check_config(){
-	### Check if configuration file exists.
-        [ ! -f $CONFIGFILE ] && close_on_error "Config file not found, make sure config file is correct"
-}
 mysqldb_backup(){
 
 	### Start database backups
         if [ "$DB_NAMES" == "ALL" ]; then
-		DATABASES=`$MYSQL $CREDENTIALS -h $MYSQL_HOST -P $MYSQL_PORT -Bse 'show databases' | grep -Ev "^(Database|mysql|performance_schema|information_schema)"$`
+		DATABASES=`$MYSQL $MYSQLCREDENTIAL -h $MYSQL_HOST -P $MYSQL_PORT -Bse 'show databases' | grep -Ev "^(Database|mysql|performance_schema|information_schema)"$`
         else
 		DATABASES=$DB_NAMES
         fi
 
         db=""
-        [ ! -d $BACKUPDIR ] && ${MKDIR} -p $BACKUPDIR
-                [ $VERBOSE -eq 1 ] && echo "*** Dumping MySQL Database ***"
-                mkdir -p ${LOCAL_BACKUP_DIR}/${CURRENT_DATE}
+        [ $VERBOSE -eq 1 ] && echo "*** Dumping MySQL Database ***"
         for db in $DATABASES
         do
-                FILE_NAME="${db}.${filenameDaily}.sql.gz"
+                FILE_NAME="${db}_${filenameDaily}.sql.gz"
                 FILENAMEPATH="$dirDaily$FILE_NAME"
                 [ $VERBOSE -eq 1 ] && echo -en "Database> $db... \n"
-                ${MYSQLDUMP} ${CREDENTIALS} --single-transaction -h ${MYSQL_HOST} -P $MYSQL_PORT $db | ${GZIP} -9 > $FILENAMEPATH
-                echo "$db   :: `du -sh ${FILENAMEPATH}`"  >> ${LOGFILENAME}
+ #               ${MYSQLDUMP} ${MYSQLCREDENTIAL} --single-transaction -h ${MYSQL_HOST} -P $MYSQL_PORT $db | ${GZIP} -9 > $FILENAMEPATH
+                echo "$db   :: `du -sh ${FILENAMEPATH}`"  >> ${logfile}
                 [ $FTP_ENABLE -eq 1 ] && ftp_backup
                 [ $SFTP_ENABLE -eq 1 ] && sftp_backup
                 [ $S3_ENABLE -eq 1 ] && s3_backup
         done
         [ $VERBOSE -eq 1 ] && echo "*** Backup completed ***"
-        [ $VERBOSE -eq 1 ] && echo "*** Check backup files in ${FILE_PATH} ***"
+        [ $VERBOSE -eq 1 ] && echo "*** Check backup files in ${dirDaily} ***"
 }
 
 
@@ -85,30 +70,38 @@ pgdb_backup(){
         db=""
         for db in $DATABASES
         do
+                errorDuringBackup=""
                 FILE_NAME="${db}.${filenameDaily}"
                 FILENAMEPATH="$dirDaily$FILE_NAME"
                 [ $VERBOSE -eq 1 ] && echo -en "Database> $db... \n"
 
-                ${PGDUMP} --host ${PG_HOST} --username $user --format=custom --file ${FILENAMEPATH} --dbname={db}
-
-                errorDuringBackup=""
+ #               ${PGDUMP} --host ${PG_HOST} --username $user --format=custom --file ${FILENAMEPATH} --dbname={db}
+touch ${FILENAMEPATH}
                 if [ $? == 0 ]; then
                         # Update monthly/yearly backups
                         cp $FILENAMEPATH $dirMonthly/$db_$filenameMonthly.dmp
                         cp $FILENAMEPATH $dirYearly/$db_$filenameYearly.dmp
+                        finalOutput="backup success"
+                        [ $FTP_ENABLE -eq 1 ] && ftp_backup
+                        [ $SFTP_ENABLE -eq 1 ] && sftp_backup
+                        [ $S3_ENABLE -eq 1 ] && s3_backup
                 else
-                errorDuringBackup='pg_dump returned non-zero code'
+                        finalOutput='backup error : pg_dump returned non-zero code'
                 fi
-                echo "$db   :: `du -sh ${FILENAMEPATH}`"  >> ${LOGFILENAME}
-                [ $FTP_ENABLE -eq 1 ] && ftp_backup
-                [ $SFTP_ENABLE -eq 1 ] && sftp_backup
-                [ $S3_ENABLE -eq 1 ] && s3_backup
-
+                echo "$db   :: `du -sh ${FILENAMEPATH}`"  >> ${logfile}
+                echo "$(date +%Y-%m-%d_%H:%M:%S) - $finalOutput" >> $logfile
         done
         [ $VERBOSE -eq 1 ] && echo "*** Backup completed ***"
-        [ $VERBOSE -eq 1 ] && echo "*** Check backup files in ${FILE_PATH} ***"
+        [ $VERBOSE -eq 1 ] && echo "*** Check backup files in ${dirDaily} ***"
 }
 
+### UTILITY FUNCTIONS  ###
+
+### Check Configuratione file exists ###
+check_config(){
+	### Check if configuration file exists.
+        [ ! -f $CONFIGFILE ] && close_on_error "Config file not found, make sure config file is correct"
+}
 ### close_on_error on demand with message ###
 close_on_error(){
         echo "$@"
@@ -142,7 +135,7 @@ check_cmds(){
 
 ### Check if database connectin is working...
 check_mysql_connection(){
-        ${MYSQLCHECK} ${CREDENTIALS} -h ${MYSQL_HOST} -P ${MYSQL_PORT} ping | ${GREP} 'alive'>/dev/null
+        ${MYSQLCHECK} ${MYSQLCREDENTIAL} -h ${MYSQL_HOST} -P ${MYSQL_PORT} ping | ${GREP} 'alive'>/dev/null
         [ $? -eq 0 ] || close_on_error "Error: Cannot connect to MySQL Server. Make sure username and password setup correctly in $CONFIGFILE"
 }
 
@@ -185,21 +178,23 @@ s3_backup(){
 ### Remove older backups
 clean_old_backups(){
 
-	[ $VERBOSE -eq 1 ] && echo "Removing old backups"
-	DBDELDATE=`date +"${DATE_FORMAT}" --date="${BACKUP_RETAIN_DAYS} days ago"`
-	if [ ! -z ${LOCAL_BACKUP_DIR} ]; then
-		  cd ${LOCAL_BACKUP_DIR}
-		  if [ ! -z ${DBDELDATE} ] && [ -d ${DBDELDATE} ]; then
-				rm -rf ${DBDELDATE}
-		  fi
-	fi
+	 [ $VERBOSE -eq 1 ] && echo "Removing old backups"
+        # keep 1 backup per day, for the last 30 days
+        find $dirDaily -type f -name "*\.zip" -mtime +30 -delete
+
+        # keep 1 backup per month, for the last 12 months (360 = 30 x 12)
+        find $dirMonthly -type f -name "*\.zip" -mtime +360 -delete
+
+        # keep 1 backup per year, for the last 10 years (3600 = 30 x 12 x 10)
+        find $dirYearly -type f -name "*\.zip" -mtime +3600 -delete
+
 }
 
 ### Send report email
 send_report(){
 	if [ $SENDEMAIL -eq 1 ]
 	then
-			cat ${LOGFILENAME} | mail -vs "Database dump report for `date +%D`" ${EMAILTO}
+			cat ${logfile} | mail -vs "Database dump report for `date +%D`" ${EMAILTO}
 	fi
 }
 
